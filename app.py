39df23a408a031b4d17a1ec3a6290a32d3ae7bf2
Chris Warwick <cnwarwick@gmail.com>
2022-11-23 11:52:54 +0000

    Made pages, intial linking of first page args

--- app.py
+++ app.py
@@ -2,14 +2,11 @@ from flask import Flask, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 import secrets
 import string
+import json
+from object_definition import options
 
 localdb = True
 app = Flask(__name__)
-
-#columns=['Item', 'Payments/year', 'First Adult', 'Subsequent Adults', 'Children'])
-EXPENSES = {'Food': [52, 50, 40, 30], 
-    'Housing': [12, 400, 300, 200], 
-    'Clothing': [12, 50, 50, 35]}
 
 app.config["DEBUG"] = True
 
@@ -42,98 +39,62 @@ class Record(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     uid = db.Column(db.String(4096))
     content = db.Column(db.String(4096))
+ 
 
-# def abort_if_value_is_not_valid(item_id, value):
+@app.route("/allresults", methods=["GET"])
+def all_results():
+    return render_template("all_results.html", records=Record.query.all())
 
-#     print(value)
-#     #some of these are probably redundant because of argparser handling
-#     if type(value) is not list:
-#         print(type(value))        
-#         abort(404, message="Item {} has invalid value {} - not list".format(item_id, value))
-
-#     if len(value) != 4:
-#         print(len(value))
-#         abort(404, message="Item {} has invalid value {} - not length 4".format(item_id, value))
-
-#     if not all(isinstance(v, int) for v in value):
-#         abort(404, message="Item {} has invalid value {} - not ints".format(item_id, value))
-
-# parser = reqparse.RequestParser()
-# for item in EXPENSES:
-#     parser.add_argument(item, type=int, action='append')
-
-
-# # ExpenseQuery
-# # shows a list of all EXPENSES, and lets you POST new values of expenses
-# class ExpenseQuery(Resource):
-#     def get(self):
-#         return EXPENSES
-
-#     def post(self):
-#         args = parser.parse_args()
-        
-#         for item in args:
-            
-#             abort_if_value_is_not_valid(item, args[item])
-
-#             EXPENSES[item] = args[item]
-
-#         #instead of returning expenses, return data regarding affordability for
-#         #households
-#         return EXPENSES, 201
-
-# # ExpenseRecord
-# # gets the data related to a single record ID 
-# class ExpenseRecord(Resource):
-#     def get(self, id):
-
-#         #check if valid id
-    
-#         #return expenses of valid id from sql database
-#         return db.session.get(Record, id)
-
-# #get all valid record IDs, or post a new one
-# class ExpenseRecordList(Resource):
-    
-#     def get(self):
-        
-#         return Record.query.all()
-
-#     def post(self):
-#         args = parser.parse_args()
-        
-#         for item in args:
-            
-#             abort_if_value_is_not_valid(item, args[item])
-
-#             EXPENSES[item] = args[item]
-        
-#         #then write expenses to sql database
-
-#         #generate long random id
-#         uid = ''.join(secrets.choice(string.ascii_uppercase + string.ascii_lowercase) for i in range(7))
-#         record = Record(content=uid)
-#         db.session.add(record)
-#         db.session.commit()
-        
-#         #return id of expense record
-#         return uid, 201        
 @app.route("/results/<uid>", methods=["GET"])
 def display_results(uid):
     record=db.session.execute(db.select(Record).filter_by(uid=uid)).one()
     return render_template("result_page.html", records=record)
 
-@app.route("/", methods=["GET", "POST"])
-def index():
+@app.route("/page3", methods=["GET", "POST"])
+def custom_control():
     if request.method == "GET":
-        return render_template("main_page.html", records=Record.query.all())
+
+        mainoption = request.args.get('mainoption', default='1')
+        return render_template("custom_control.html", startValues=mainoption)
 
     uid = ''.join(secrets.choice(string.ascii_uppercase + string.ascii_lowercase) for i in range(7))
     record = Record(uid=uid, content=request.form["contents"])
     db.session.add(record)
     db.session.commit()
-    return redirect(url_for('index'))
+    return redirect(url_for('display_results', uid=uid))
 
+@app.route("/page2", methods=["GET", "POST"])
+def main_control():
+    if request.method == "GET":
+
+        n_adults = int(request.args.get('nadult', default='2'))
+        n_children = int(request.args.get('nchild', default='0'))
+
+        for opt in options:
+            opt['value']=opt['adult1'] + (n_adults-1)*opt['adult2'] + n_children*opt['child']
+
+        return render_template("main_control.html", options=json.dumps(options))
+
+    mainoption=request.form["mainoption"]
+
+    return redirect(url_for('custom_control') + '?mainoption=' + mainoption)
+
+@app.route("/page1", methods=["GET", "POST"])
+def user_info():
+    if request.method == "GET":
+        return render_template("user_info.html")
+
+    n_children=request.form["n_children"]
+    n_adults=request.form["n_adults"]
+    #do something with request.form
+    return redirect(url_for('main_control') + '?nadult=' + n_adults + '&nchild='+ n_children )
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "GET":
+        return render_template("landing.html")
+
+    return redirect(url_for('user_info'))
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ object_definition.py
@@ -0,0 +1,13 @@
+
+op1 = {'name' : 'Survive', 'adult1': 100, 'adult2':50, 'child':50, 'description': 'asdasdasd'}
+op2 = {'name' : 'Modern essentials', 'adult1': 100, 'adult2':50, 'child':50, 'description': 'asdasdasd'}
+op3 = {'name' : 'Basic comforts', 'adult1': 100, 'adult2':50, 'child':50, 'description': 'asdasdasd'}
+op4 = {'name' : 'Little luxuries', 'adult1': 100, 'adult2':50, 'child':50, 'description': 'asdasdasd'}
+op5 = {'name' : 'Larger luxuries', 'adult1': 100, 'adult2':50, 'child':50, 'description': 'asdasdasd'}
+options = [op1, op2, op3, op4, op5]
+#options = ['Survive','Modern essentials','Basic comforts','Little luxuries','Larger luxuries']
+
+#columns=['Item', 'Payments/year', 'First Adult', 'Subsequent Adults', 'Children'])
+EXPENSES = {'Food': [52, 50, 40, 30], 
+    'Housing': [12, 400, 300, 200], 
+    'Clothing': [12, 50, 50, 35]}
